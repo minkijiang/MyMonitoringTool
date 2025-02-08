1829a8e1e6b037de22f3e6e07b285ee8a1efb916
minkijiang <jiangminki0@gmail.com>
2025-02-07 19:17:11 -0500

    Add files via upload

--- m.c
+++ m.c
@@ -2,13 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <time.h>
 
 int main() {
 
 
   int c = log10(4);
 
+  int nano = 900000000;
+  int sec = 1;
+
+  struct timespec sleep_time;
+  sleep_time.tv_sec = sec;  
+  sleep_time.tv_nsec = nano; 
+
+  nanosleep(&sleep_time, NULL);
+
   return 0;
 }
 
-//gcc --std=c99 m.c -lm 
+//gcc --std=c99 m.c -lrt -lm 
